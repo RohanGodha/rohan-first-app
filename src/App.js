e9d860e2d35c59bb22a5475d35cd0588fee785e4
Rohan Godha <yoyorodha@gmail.com>
2023-07-18 15:36:19 +0530

    Added HOC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import ClassComponent from "./components/ClassComponent";
 import FunctionalComponent from "./components/FunctionalComponent";
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+
 let age = 22;
 let college = "Chandigarh University";
 
 function App() {
   const [name, setName] = useState("Rohan");
   return (
-    <div className="App">
-      <h1>Class Based Component</h1>
-      <ClassComponent />
-      <br />
-      <h1>Functional Component</h1>
-      <FunctionalComponent
-        name={name}
-        age={age}
-        college={college}
-        setName={setName}
+    <Routes>
+      <Route path="/" element={<ClassComponent />} />
+      <Route
+        path="/functional"
+        element={
+          <FunctionalComponent
+            name={name}
+            age={age}
+            college={college}
+            setName={setName}
+          />
+        }
       />
-      <br />
-    </div>
+    </Routes>
+
+    // <div className="App">
+    //   <h1>Class Based Component</h1>
+    //   <ClassComponent />
+    //   <br />
+    //   <h1>Functional Component</h1>
+    //   <FunctionalComponent
+    //     name={name}
+    //     age={age}
+    //     college={college}
+    //     setName={setName}
+    //   />
+    //   <br />
+    // </div>
   );
 }
 
